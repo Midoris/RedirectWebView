120ab2ea8bad735c1b2d9e7bbcca2cf7756c6fec
Iablonskyi Ievgenii <eugen_yablonski@yahoo.com>
2020-03-11 16:54:19 +0800

    Initial Commit

---
+++ WebViewRedirect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebViewRedirect
+//
+//  Created by IEVGENII IABLONSKYI on 11.03.20.
+//  Copyright © 2020 IEVGENII IABLONSKYI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
